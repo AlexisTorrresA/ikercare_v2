a776e430716b5534a16a864b3d92f63169ab722b
Alexis Torres <alexis.torres.alvarez@gmail.com>
2026-08-09 23:59:39 -0400

    Load chemo and dialog fix without other changes

--- app/static/v2.js
+++ app/static/v2.js
@@ -12,12 +12,13 @@
     });
   }
 
-  loadScript("/static/v2-core.js?v=2.0.7")
-    .then(() => loadScript("/static/v2-nav.js?v=2.0.7"))
-    .then(() => loadScript("/static/v2-visual.js?v=2.0.7"))
-    .then(() => loadScript("/static/v2-bugfixes.js?v=2.0.7"))
-    .then(() => loadScript("/static/v2-patient-actions.js?v=2.0.7"))
-    .then(() => loadScript("/static/v2-record-actions.js?v=2.0.7"))
-    .then(() => loadScript("/static/v2-local-date-fix.js?v=2.0.7"))
+  loadScript("/static/v2-core.js?v=2.0.8")
+    .then(() => loadScript("/static/v2-nav.js?v=2.0.8"))
+    .then(() => loadScript("/static/v2-visual.js?v=2.0.8"))
+    .then(() => loadScript("/static/v2-bugfixes.js?v=2.0.8"))
+    .then(() => loadScript("/static/v2-patient-actions.js?v=2.0.8"))
+    .then(() => loadScript("/static/v2-record-actions.js?v=2.0.8"))
+    .then(() => loadScript("/static/v2-local-date-fix.js?v=2.0.8"))
+    .then(() => loadScript("/static/v2-chemo-dialog-fixes.js?v=2.0.8"))
     .catch(error => console.error("No se pudo cargar IkerCare 2", error));
 })();
