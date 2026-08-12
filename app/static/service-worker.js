2813f955c25f4b845dd3904f5b5a094d589293af
Alexis Torres <alexis.torres.alvarez@gmail.com>
2026-08-12 10:58:38 -0400

    Actualizar caché para edición de medicamentos

--- app/static/service-worker.js
+++ app/static/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ikercare-static-v2.0.0-v2.3.7';
+const CACHE_NAME = 'ikercare-static-v2.0.0-v2.3.8';
 const STATIC_ASSETS = [
   '/static/v2.css',
   '/static/v2.js',
@@ -14,6 +14,7 @@ const STATIC_ASSETS = [
   '/static/v2-chemo-order-report.js',
   '/static/v2-dialog-refresh-guard.js',
   '/static/v2-food-registration-fix.js',
+  '/static/v2-medication-edit-fix.js',
   '/static/manifest.webmanifest',
   '/static/icons/favicon-64.png',
   '/static/icons/icon-192.png',
