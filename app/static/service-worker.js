c19180890ef57b91cb2492b501989b70b93051aa
Alexis Torres <alexis.torres.alvarez@gmail.com>
2026-08-12 01:29:33 -0400

    Refresh cache for SOS today controls

--- app/static/service-worker.js
+++ app/static/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ikercare-static-v2.0.0-v2.2.4';
+const CACHE_NAME = 'ikercare-static-v2.0.0-v2.2.5';
 const STATIC_ASSETS = [
   '/static/v2.css',
   '/static/v2.js',
@@ -9,6 +9,7 @@ const STATIC_ASSETS = [
   '/static/v2-visible-care-fixes.js',
   '/static/v2-visible-care-actions.js',
   '/static/v2-medication-extras.js',
+  '/static/v2-sos-today.js',
   '/static/manifest.webmanifest',
   '/static/icons/favicon-64.png',
   '/static/icons/icon-192.png',
